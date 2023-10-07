0c1225e487d024d7595bf5e7a9cbb367fe04dbbf
Utochka132 <127588363+Utochka132@users.noreply.github.com>
2023-10-07 09:46:10 +0400

    Add files via upload

---
+++ Ali.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from Dop_Ali import get_class
+import os, random
+import requests
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.command()
+async def check_image(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            file_name = attachment.filename
+            file_url = attachment.url
+            await attachment.save(f"./{attachment.filename}")
+            await ctx.send(get_class(model_path="./keras_model.h5", labels_path="labels.txt", image_path=f"./{attachment.filename}"))
+    else:
+        await ctx.send("Вы забыли загрузить картинку :(")
+
+
+bot.run("ТУТ СЕКРЕТНЫЙ ТОКЕН")
